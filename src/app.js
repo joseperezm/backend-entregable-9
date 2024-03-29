7afb7741926232505c95d9d989881a4fd5769934
joseperezm <git@jjpm.cl>
2024-03-29 00:06:07 -0300

    Primer commit

---
+++ src/app.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const flash = require('connect-flash');
+const session = require('express-session');
+const exphbs = require("express-handlebars");
+const socket = require("socket.io");
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const PUERTO = 8080;
+const productsRouter = require("./routes/products.router.js");
+const cartsRouter = require("./routes/carts.router.js");
+const viewsRouter = require("./routes/views.router.js");
+const sessionsRouter = require('./routes/sessions.router.js');
+const initializePassport = require("./config/passport.config.js");
+
+const app = express();
+
+const environment = process.env.NODE_ENV || 'development';
+const envPath = environment === 'development' ? '.env.development' : '.env.production';
+
+require('dotenv').config({ path: envPath });
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Conectado a MongoDB'))
+    .catch(err => console.error('Error al conectar a MongoDB', err));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+const oneWeekLogin = 7 * 24 * 60 * 60;
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI,
+        ttl: oneWeekLogin
+    }),
+    cookie: {
+        maxAge: oneWeekLogin * 1000 // milisegundos
+    }
+}));
+
+initializePassport();
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use((req, res, next) => {
+    if (req.session.user) {
+        res.locals.user = req.session.user;
+    }
+    next();
+});
+
+app.use(express.static("./src/public"));
+
+app.engine("handlebars", exphbs.engine({
+    helpers: {
+        multiply: function (a, b) {
+            return (a * b).toFixed(2);
+        },
+        totalCart: function (products) {
+            let total = 0;
+            products.forEach(product => {
+                total += product.quantity * product.productId.price;
+            });
+            return total.toFixed(2);
+        }
+    }
+}));
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
+
+app.use("/api", productsRouter);
+app.use("/api", cartsRouter);
+app.use("/", viewsRouter);
+
+app.use('/api/sessions', sessionsRouter);
+
+const server = app.listen(PUERTO, () => {
+    console.log(`Servidor escuchando en el puerto ${PUERTO}`);
+});
+
+const ProductManagerFs = require("./dao/fs/productManager-fs")
+const productManagerFs = new ProductManagerFs("./dao/fs/products.json");
+
+const ProductManager = require("./dao/db/productManager.js");
+const productManager = new ProductManager();
+
+const io = socket(server);
+
+io.on("connection", async (socket) => {
+    console.log("Nuevo cliente conectado");
+
+    socket.emit("products", await productManager.getProducts());
+
+    socket.on("deleteProduct", async (id) => {
+        await productManager.deleteProduct(id);
+        io.sockets.emit("products", await productManager.getProducts());
+    });
+
+    socket.on("addProduct", async (producto) => {
+        await productManager.addProduct(producto);
+        io.sockets.emit("products", await productManager.getProducts());
+    });
+});
+
+const Message = require('./dao/models/messages-mongoose.js');
+
+io.on('connection', (socket) => {
+    console.log('Un usuario se ha conectado');
+
+    socket.on('user email provided', (email) => {
+        console.log(`Correo electrónico recibido: ${email}`);
+
+        Message.find().then(messages => {
+            socket.emit('load all messages', messages);
+        });
+    });
+
+    socket.on('chat message', async (data) => {
+        try {
+            const message = new Message(data);
+            await message.save();
+            io.emit('chat message', data);
+        } catch (error) {
+            console.error('Error guardando el mensaje', error);
+        }
+    });
+
+});
---
+++ src/routes/products.router.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const router = express.Router();
+
+const ProductManagerFs = require("../dao/fs/productManager-fs")
+const productManagerFs = new ProductManagerFs("../dao/fs/products.json");
+
+const ProductManager = require("../dao/db/productManager");
+const productManager = new ProductManager();
+
+const redirectIfNotLoggedInApi = require('../middleware/authApi.js');
+
+router.get('/products', redirectIfNotLoggedInApi, async (req, res) => {
+    const limit = req.query.limit === undefined ? 10 : parseInt(req.query.limit, 10);
+    const page = parseInt(req.query.page) || 1;
+    const sort = req.query.sort;
+    const query = req.query.query; 
+    
+    try {
+        const { products, totalPages } = await productManager.getProducts({ limit, page, sort, query });
+
+        const hasPrevPage = page > 1;
+        const hasNextPage = page < totalPages;
+        const prevPage = hasPrevPage ? page - 1 : null;
+        const nextPage = hasNextPage ? page + 1 : null;
+        const baseUrl = '/products?';
+
+        res.json({
+            status: "success",
+            payload: products,
+            totalPages,
+            prevPage,
+            nextPage,
+            page,
+            hasPrevPage,
+            hasNextPage,
+            prevLink: hasPrevPage ? `${baseUrl}page=${prevPage}` : null,
+            nextLink: hasNextPage ? `${baseUrl}page=${nextPage}` : null
+        });
+    } catch (error) {
+        console.error('Error obteniendo los productos:', error);
+        res.status(500).json({ status: "error", message: error.message });
+    }
+});
+
+router.get('/products/:pid', redirectIfNotLoggedInApi, async (req, res) => {
+    try {
+        const product = await productManager.getProductById(req.params.pid);
+
+        if (product) {
+            res.json(product);
+        } else {
+            res.status(404).json({ message: 'Producto no encontrado o inexistente' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.post('/products', redirectIfNotLoggedInApi, async (req, res) => {
+    try {
+        const newProduct = req.body;
+        const product = await productManager.addProduct(newProduct);
+        res.status(201).json({ id: product._id, message: 'Producto agregado correctamente' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.put('/products/:id', redirectIfNotLoggedInApi, async (req, res) => {
+    try {
+        const updatedProduct = await productManager.updateProduct(req.params.id, req.body);
+
+        if (updatedProduct) {
+            res.json({ message: 'Producto actualizado correctamente', product: updatedProduct });
+        } else {
+            res.status(404).json({ message: 'Producto no encontrado o inexistente' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.delete('/products/:pid', redirectIfNotLoggedInApi, async (req, res) => {
+    try {
+        const success = await productManager.deleteProduct(req.params.pid);
+        
+        if (success) {
+            res.status(200).json({ message: 'Producto eliminado correctamente' });
+        } else {
+            res.status(404).json({ message: 'El producto que desea eliminar no existe' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = router;
